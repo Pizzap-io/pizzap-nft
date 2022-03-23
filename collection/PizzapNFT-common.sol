6d29a29949a21cfcddca79aadd72ccd2bacaf51f
richard <renyanduo@163.com>
2022-03-23 09:53:11 +0800

    update sol version

--- collection/PizzapNFT-common.sol
+++ collection/PizzapNFT-common.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 
 
-pragma solidity ^0.8.2;
+pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
